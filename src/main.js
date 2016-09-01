5841262b4565e64f42d4fed4534a9e1abfa2789d
YU <fan.yu@philips.com>
2016-09-01 16:46:08 +0800

    update

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {render} from 'react-dom'
+import routes from './routes'
+import {Router,hashHistory} from 'react-router'
+
+render((<Router history={hashHistory} routes={routes}/>),
+    document.getElementById("container")
+)
